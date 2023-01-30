2a4df561fc7ee7f85ec31a01bf1fce33cc7e73e3
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-01-30 21:48:05 +0500

    first commit of cmbe1

--- models/Contact.js
+++ models/Contact.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const ContactSchema = mongoose.Schema({
-    // user: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'users'
-    // },
+     user: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'users'
+     },
     name: {
         type: String,
         require: true
