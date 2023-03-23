1adfee62d474f942ac2456d85c61d3b51c4c62e4
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-03-23 23:34:18 +0500

    BE folder resources being uploaded to github

--- models/Contact.js
+++ models/Contact.js
@@ -25,37 +25,3 @@ const ContactSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model('contact', ContactSchema);
-
-
-//********************* */
-
-// const mongoose = require('mongoose');
-
-// const ContactSchema = mongoose.Schema({
-//     user: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'users'
-//     },
-//     name: {
-//         type: String,
-//         require: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//     },
-//     phone: {
-//         type: String,
-//         required: true,
-//     },
-//     type: {
-//         type: String,
-//         default: 'Personal'
-//     },
-//     date: {
-//         type: Date,
-//         default: Date.now
-//     }
-// });
-
-// module.exports = mongoose.model('contact', ContactSchema);
